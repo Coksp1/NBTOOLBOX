2a422ac86074acf859a5a9e0d1809dfdcd117841
Kenneth Sæterhagen Paulsen <45991834+Coksp1@users.noreply.github.com>
2021-09-03 12:52:20 +0200

    Add files via upload

--- Examples/Econometrics/DSGE/Non_stationary_model/JPT/lookUpMatrixJPT.m
+++ Examples/Econometrics/DSGE/Non_stationary_model/JPT/lookUpMatrixJPT.m
@@ -7,12 +7,16 @@
 %   - third column  : The norwegian text to match the given expression
 %--------------------------------------------------------------------------
 obj.lookUpMatrix = {
-'C_NW',             'Consumption',                      'Konsum';
+'C_NW',             'Consumption gap',                  'Konsumgap';
 'DPQ_P_NW',         'Inflation',                        'Inflasjon';
 'DPQ_W_NW',         'Wage inflation',                   'Lønnsinflasjon';
-'I_NW',             'Investment',                       'Investeringer';   
-'K_NW',             'Capital',                          'Kapital';
+'I_NW',             'Investment gap',                   'Investeringsgap';   
+'K_NW',             'Capital gap',                      'Kapitalgap';
 'L_NW',             'Hours worked',                     'Timeverk';
-'NAT_Y_NW',         'Output',                           'Produksjons';
+'NAT_Y_NW',         'Output gap',                       'Produksjonsgap';
 'RN3M_NW',          'Money market interest rate',       'Pengemarkedsrenten';
+'C_NW_LEVEL',       'Consumption',                      'Konsum';
+'I_NW_LEVEL',       'Investment',                       'Investeringer';
+'K_NW_LEVEL',       'Capital',                          'Kapital';
+'Y_NW_LEVEL',       'Output',                           'Produksjon';
 };
